c39f60a4513a0fbcc31ad58ebf3390bbfb2cd623
Pablo Suárez Pérez <brekopablo@gmail.com>
2024-10-29 05:21:46 +0800

    events

--- sections/add_space.py
+++ sections/add_space.py
@@ -11,13 +11,13 @@ from utils.firebase import Firebase
 db = Firebase().getdb()
 
 def app():
-    st.title("Add Spaces to your Places")
+    st.title("Your Businesses")
+    places = []
     lugares = db.child('Lugares').get().val()
-    places=list()
     for l in lugares:
         lugar = db.child('Lugares').child(l).child('owner').get().val()
-        if lugar == st.session_state.user:
-            places.append(lugar)
+        if st.session_state.ID == lugar:
+            places.append(l)
     places = list(set(places))
     selected_option = st.radio("What type of user are you?", ("Event", "Space"))
     if selected_option == 'Event':
@@ -28,21 +28,32 @@ def app():
 
 def space_register(places):
     st.title("Add a Space")
-    st.write("Select the place where you want to add a space")
-    selected_place = st.selectbox("Places", places)
+    selected_place = st.selectbox("Select the place where you want to add a space", places)
     st.write("Select the type of space you want to add")
-    selected_type = st.text_input("Example: Kitchen")
-    st.write("Ocupability")
-    occp = st.text_input("10")
-    st.write("Status")
-    status = st.text_input("Busy")
+    selected_type = st.text_input("Select the type of space you want to add")
+    occp = st.text_input("Ocupability")
+    status = st.text_input("Status")
     submit = st.button("Create Space")
     # Enviar información.
     if submit:          
-        db.child("Lugares").child(selected_place).child('Place').set(selected_type)
-        db.child("Lugares").child(selected_place).child('x').set(occp)
-        db.child("Lugares").child(selected_place).child('y').set(status)
+        db.child("Lugares").child(selected_place).child('spaces').child(selected_type).set(selected_type)
+        db.child("Lugares").child(selected_place).child('spaces').child(selected_type).child('occp').set(occp)
+        db.child("Lugares").child(selected_place).child('spaces').child(selected_type).child('status').set(status)
         st.success('The place has been created successfully.')
         st.balloons()
 def event_register(places):
-    pass
+    st.title("Add a Event")
+    selected_place = st.selectbox("Select the place where you want to add a space", places)
+    spaces = places.child('spaces')
+    selected_spaces = st.selectbox("Select the place where you want to add a space", spaces)
+    name_event = st.text_input("Write the name of the event")
+    occp = st.text_input("Ocupability")
+    status = st.text_input("Status")
+    submit = st.button("Create Space")
+    # Enviar información.
+    if submit:          
+        db.child("Lugares").child(selected_place).child('events').child(name_event).set(name_event)
+        db.child("Lugares").child(selected_place).child('events').child(name_event).child('selected_spaces').set(selected_spaces)
+        db.child("Lugares").child(selected_place).child('events').child(name_event).child('occp').set(occp)
+        st.success('The place has been created successfully.')
+        st.balloons()
